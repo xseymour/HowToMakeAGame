1b5cdcc35f6997d9cebc736ffbef02b8fef9d90d
Xavier Seymour <xseymour3@gmail.com>
2018-04-25 18:26:06 -0400

    Episode 3: Movement
    Basic Player Movement using keyboard keys

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,41 +5,35 @@ namespace Assets
 
 	public class PlayerMovement : MonoBehaviour
 	{
+		public Rigidbody rb;
+		public float forwardForce	= 1000f;
+		public float sidewaysForce	= 600f;
 
-		//Task 1: Disable Gravity for our player object
-		public Rigidbody Rb;
-
-
-
-
-
-		// Use this for initialization
-		public void Start()
-		{
-//			Debug.Log("Hello World!");
-//			Rb.useGravity = false;
-//			Rb.AddForce(0, 200, 0);
-
-		}
-
-		// Update is called once per frame
-		public void Update()
-		{
-
-		}
-
-
-		/*
+		/*	
 		 * Update is called once per frame
 		 * Use FixedUpdate for anything involving unity physics update (forces, velocities, etc.)
 		 * Makes things smoother during collisons
+		 * Todo Fixed Update not good for one off events such as jumping. Fixed update does not run as often as update method,
+		 * Todo so input lag is introduced. Correct method is to store input in update method, then use variables in fixed update for physics
 		 */
 		public void FixedUpdate()
 		{
+			float delta = Time.deltaTime;
 			// Todo Error: This MAY add a force of 200 every frame, resulting in different framerates causing different forces
-			//			Rb.AddForce(0, 0, 200);
+			//			rb.AddForce(0, 0, 200);
 			// I suppose this might be better, so as to get a certain force per second
-			Rb.AddForce(0, 0, 2000 * Time.deltaTime);
+			rb.AddForce(0, 0, forwardForce * delta);		
+
+			if (Input.GetKey("d") || Input.GetKey("right"))
+			{
+				rb.AddForce(sidewaysForce * delta, 0, 0);
+			}
+
+			if (Input.GetKey("a") || Input.GetKey("left"))
+			{
+				rb.AddForce(-sidewaysForce * delta, 0, 0);
+			}
+
 		}
 
 	}
