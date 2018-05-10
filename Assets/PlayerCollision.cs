88573646f0965483fb06c350dc743a908ec9e137
Xavier Seymour <xseymour3@gmail.com>
2018-05-09 23:07:05 -0400

    Episodes 4 + 5
    Camera Follow + Player Collision

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using Assets;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+	public PlayerMovement movement;
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		if (collisionInfo.collider.tag == "Obstacle")
+		{
+			movement.enabled = false;
+		}
+	}
+
+}
